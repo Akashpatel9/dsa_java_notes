bd314241eb279d6c20ba877f04ea3f4281700881
akash <akashpatelsingh9893693091@gmail.com>
2023-03-20 01:02:56 +0530

    Initial commit

--- src/DSA_notes/sorting/Prime_Sieve_of_Eratosthenes.java
+++ src/DSA_notes/sorting/Prime_Sieve_of_Eratosthenes.java
@@ -1,34 +1,26 @@
 package DSA_notes.sorting;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.util.*;
 public class Prime_Sieve_of_Eratosthenes {
-    public Prime_Sieve_of_Eratosthenes() {
-    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter range to print no");
-        int n = Integer.parseInt(br.readLine());
+        int n = sc.nextInt();
+
         boolean[] arr = new boolean[n + 1];
+
         Arrays.fill(arr, true);
+
         arr[0] = false;
         arr[1] = false;
 
-        int i;
-        for(i = 2; (double)i < Math.sqrt((double)arr.length); ++i) {
+        for(int i = 2; (double)i < Math.sqrt((double)arr.length); i++) {
             for(int j = i * 2; j < arr.length; j += i) {
                 arr[j] = false;
             }
         }
 
-        for(i = 0; i < arr.length; ++i) {
-            if (arr[i]) {
-                System.out.print("" + i + " ");
-            }
+        for(int i = 2; i < arr.length; ++i) {
+            if (arr[i]) System.out.print(i+" ");
         }
 
     }
--- src/pra.java
+++ src/pra.java
@@ -1,4 +1,4 @@
-public class pra {
+class pra{
     public static void main(String[] args) {
 
     }
